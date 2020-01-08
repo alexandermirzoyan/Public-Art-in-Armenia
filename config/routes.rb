0f861180156b317fd54310e48045a5a2023c2278
Alexander <alexandr.mirz12@gmail.com>
2020-01-08 18:16:59 +0400

    Root page is ArtList, without logging in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'posts#landing_page'
   get 'static_pages/help'
   get 'static_pages/post'
-  resources :posts
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
